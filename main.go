1a21b8cf16bbd3c74c8e3893da68371b23f6a097
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-10-20 10:43:14 +0900

    accept command line arguments

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
 )
+
+func main() {
+	dataPath := flag.String("data", "./data", "specify response dir")
+	port := flag.String("port", "8080", "specify port")
+	flag.Parse()
+	paths := makePaths(*dataPath)
+	fmt.Println(paths)
+	fmt.Println(port)
+}
 
 func registerEndpoints(mux *http.ServeMux, paths []string) {
 	//print file
@@ -15,7 +25,12 @@ func registerEndpoints(mux *http.ServeMux, paths []string) {
 		if error != nil {
 			die("path doesn't exist: %s", path)
 		}
-		fmt.Println(string(data[:]))
+		//		fmt.Println(string(data[:]))
+		mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println(data)
+		})
+
+		//set
 	}
 
 	//set contents type
@@ -23,6 +38,13 @@ func registerEndpoints(mux *http.ServeMux, paths []string) {
 }
 
 func makePaths(path string) []string {
+	fi, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		die("not exists: %s", path)
+	}
+	if !fi.Mode().IsDir() {
+		die("not dir: %s", path)
+	}
 	var paths []string
 	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
